e35d8cd9e74944bdf16828e19c1c6ab9e1515c65
Lalit Verma <lalitv94r@gmail.com>
2025-08-22 22:44:07 +0530

    Add default

--- src/app/(MainBody)/bonusui/pagination/page.tsx
+++ src/app/(MainBody)/bonusui/pagination/page.tsx
@@ -2,7 +2,10 @@ import PaginationContainer from "@/Components/BonusUi/Pagination/Pagination";
 import React from "react";
 
 const Pagination = () => {
-  return <PaginationContainer />;
+  return <PaginationContainer activePage={1}
+    pages={12}
+    onActivePageChange={(num: any) => console.log(num)}
+  />;
 };
 
 export default Pagination;
